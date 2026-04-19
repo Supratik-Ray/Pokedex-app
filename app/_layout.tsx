b6c13e78e701f4178637114c9b9c4bf2eaa72e6d
Supratik Ray <raysupratik8@gmail.com>
2026-04-19 17:15:13 +0530

    added bottom sheet and tab view inside bottom sheet in pokemon details page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Pokedex" }} />
-    </Stack>
+    <>
+      <StatusBar barStyle="dark-content" />
+
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Pokedex" }} />
+        <Stack.Screen name="details" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
--- app/details.tsx
+++ app/details.tsx
@@ -1,14 +1,136 @@
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TabBar, TabView } from "react-native-tab-view";
+
+interface PokemonDetails {
+  crySound: string;
+}
+
+const AboutRoute = () => (
+  <View>
+    <Text>About Content</Text>
+  </View>
+);
+
+const StatsRoute = () => (
+  <View>
+    <Text>Stats Content</Text>
+  </View>
+);
+const MovesRoute = () => (
+  <View>
+    <Text>Stats Content</Text>
+  </View>
+);
+const EvolutionRoute = () => (
+  <View>
+    <Text>Stats Content</Text>
+  </View>
+);
 
 export default function Details() {
-  const params = useLocalSearchParams();
+  const [pokemonDetails, setPokemonDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useLocalSearchParams<{ name: string }>();
+  const layout = useWindowDimensions();
+
+  //bottom-sheet
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  //tab-view
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "about", title: "About" },
+    { key: "stats", title: "Stats" },
+    { key: "evolution", title: "Evolution" },
+    { key: "moves", title: "Moves" },
+  ]);
+
+  const renderScene = ({ route }: { route: { key: string } }) => {
+    switch (route.key) {
+      case "about":
+        return <AboutRoute />;
+      case "stats":
+        return <StatsRoute />;
+      case "evolution":
+        return <EvolutionRoute />;
+      case "moves":
+        return <MovesRoute />;
+      default:
+        return null;
+    }
+  };
+
+  useEffect(() => {
+    try {
+      async function fetchPokemonDetails() {
+        // const response = await fetch(
+        //   `https://pokeapi.co/api/v2/pokemon/${params.name}`,
+        // );
+        // const data = await response.json();
+      }
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
   return (
-    <View>
-      <Text>{params.name}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View>
+          <Text>{params.name}</Text>
+        </View>
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={["50%", "70%"]}
+          index={1}
+          enablePanDownToClose={false}
+          handleIndicatorStyle={{ display: "none" }}
+          backgroundStyle={{
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+          }}
+          enableContentPanningGesture={false}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 16 }}>
+            <View style={{ flex: 1 }}>
+              <TabView
+                style={{ height: 400 }}
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                swipeEnabled={true}
+                renderTabBar={(props) => (
+                  <TabBar
+                    {...props}
+                    style={{
+                      backgroundColor: "white",
+                      elevation: 0,
+                      shadowOpacity: 0,
+                    }}
+                    activeColor="black"
+                    inactiveColor="gray"
+                    indicatorStyle={{
+                      backgroundColor: "purple",
+                      height: 3,
+                      borderRadius: 50,
+                    }}
+                  />
+                )}
+              />
+            </View>
+          </BottomSheetView>
+        </BottomSheet>
+      </GestureHandlerRootView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16 },
+});
